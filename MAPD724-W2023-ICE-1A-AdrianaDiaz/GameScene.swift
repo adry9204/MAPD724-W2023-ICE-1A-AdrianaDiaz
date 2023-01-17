d5bb244282aafb5e18d9e71120e8c2a2d9b1be33
adry9204 <adriana920420@gmail.com>
2023-01-17 01:01:32 -0500

    Ocean GameObject Implemented Motion Working

--- MAPD724-W2023-ICE-1A-AdrianaDiaz/GameScene.swift
+++ MAPD724-W2023-ICE-1A-AdrianaDiaz/GameScene.swift
@@ -3,8 +3,24 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ocean1 : Ocean?
+    var ocean2 : Ocean?
+    
     override func sceneDidLoad() {
-
+        name = "GAME"
+        
+        // add ocean1 to the scene and starts it at the Reset location
+        ocean1 = Ocean()
+        ocean1?.Start()
+        ocean1?.Reset()
+        addChild(ocean1!)
+        
+        // add ocean2 to the scene and starts it lower
+        ocean2 = Ocean()
+        ocean2?.Start()
+        ocean2?.position.y = -773
+        addChild(ocean2!)
+                
     }
     
     
@@ -21,23 +37,26 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-       
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-       
+    // triggered every frame - once every 16ms
+    override func update(_ currentTime: TimeInterval)
+    {
+        ocean1?.Update()
+        ocean2?.Update()
     }
 }
